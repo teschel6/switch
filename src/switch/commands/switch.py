c3282e5586117c84f5665ac2e1d91f497f962f6d
etesch <elijah.tesch@gmail.com>
2025-12-09 18:37:57 -0700

    run activation script

--- src/switch/commands/switch.py
+++ src/switch/commands/switch.py
@@ -1,14 +1,27 @@
 import os
 import subprocess
 from pathlib import Path
+
 from switch import utils
 
 
 def switch():
     selected = utils.select_project()
+    project = utils.load_project(selected.directory)
+    editor = utils.get_editior()
 
     print(f"selected '{selected.name}' switching to '{selected.directory}'")
-
-    # TODO: find better way to open project
     os.chdir(Path(selected.directory))
-    subprocess.run(["bash"])
+
+    if project.activate is not None:
+        shell = os.environ.get("SHELL", "/bin/bash")
+        activate_cmd = " ".join(project.activate)
+
+        print(f"activating project with '{activate_cmd}'")
+        print(f"opening in editor '{editor}' ...")
+
+        full_command = f"{activate_cmd} && {editor} ."
+        subprocess.run([shell, "-c", full_command])
+    else:
+        print("opening in editor '{editor}' ...")
+        subprocess.run([editor, "."])
